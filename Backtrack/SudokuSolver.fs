fe05b641d17f6b17e5456f8152cb403d706418e6
asik <blask_prawdi@yahoo.ca>
2014-05-28 00:35:43 -0400

    Some performance improvements

--- Backtrack/SudokuSolver.fs
+++ Backtrack/SudokuSolver.fs
@@ -1,5 +1,6 @@
 ﻿module SudokuSolver
 
+open Microsoft.FSharp.NativeInterop
 // The general approach taken here is a straightforward application of the backtrack algorithm.
 // This is a very efficient optimisation on the brute-force algorithm, where we proceed
 // by adding one element at a time and backing out early (backtracking) as soon as an invalid
@@ -21,10 +22,11 @@ let backtrack root reject accept first next =
     let rec bt c = seq {
         if not (reject c) then
             if accept c then yield c
-            else
-                let f = first c
-                for s in f |> Seq.unfold (fun s -> if s = None then None else Some(s, (next s.Value))) do
-                    yield! bt(s.Value)
+            let f = first c
+            for s in f |> Seq.unfold (function 
+                | None -> None 
+                | Some(value) -> Some(Some(value), (next value))) do
+                yield! bt(s.Value)
     }
             
     bt root
@@ -37,32 +39,35 @@ let groupDimension = 3
 let getIndex x y = y * boardDimension + x
 
 /// Whether every square of the board has a valid (i.e. non-zero) value written to
-let boardComplete board =
-    not (board |> Array.exists (fun elem -> elem = 0))
+let boardComplete = 
+    Array.forall (fun elem -> elem > 0)
 
-/// Whether the collection of squares refered by coords contains the same element twice or more
-/// Interesting optimisation here: instead of naively comparing each element with all the others,
-/// we map every value to a bool array indicating whether it's been encountered before. This makes
-/// the algorithm O(N) instead of O(N^2) and much faster.
-let containsDuplicates coords (board:int[]) =
-    let found = Array.zeroCreate (boardDimension + 1)
-    coords |> Array.exists(fun index ->
-        let elem = board.[index]
-        let exists = elem > 0 && found.[elem]
-        found.[elem] <- true
-        exists)
+/// Whether the collection of squares refered by coords contains the same element twice or more.
+/// This function needs to be heavily optimised because it is here that we spend most time.
+/// Optimisation #1: instead of naively comparing each element with all the others,
+/// we map every value to a bool array indicating whether it's been encountered before.
+/// Optimisation #2: we allocate the array on the stack and index it using unsafe code.
+#nowarn "9"
+let containsDuplicates (coords:int[]) (board:int[]) =
+    let found = NativePtr.stackalloc<bool>(boardDimension + 1) 
+    let mutable exists = false
+    for i = 0 to coords.Length - 1 do
+        let elem = board.[coords.[i]]
+        exists <- exists || (elem > 0 && NativePtr.get found elem)
+        NativePtr.set found elem true
+    exists
 
 /// An array of arrays of coordinates each refencering all squares of a line
 let lineCoords = [| 
     for y in 0 .. boardDimension - 1 ->
         [| for x in 0 .. boardDimension - 1 ->
-            getIndex x y |] |]
+            getIndex x y |]|]
 
 /// An array of arrays of coordinates each refencering all squares of a column
 let columnCoords = [| 
     for x in 0 .. boardDimension - 1 ->
         [| for y in 0 .. boardDimension - 1 ->
-            getIndex x y |] |]
+            getIndex x y |]|]
 
 /// An array of arrays of coordinates each refencering all squares of a group
 let groupCoords = [|
@@ -74,8 +79,8 @@ let groupCoords = [|
                         getIndex x y |]|]
 
 /// Whether there are no duplicates alongside the coordinate arrays
-let hasNoDuplicatesIn board coords =
-    not (coords |> Array.exists(fun line -> containsDuplicates line board))
+let hasNoDuplicatesIn board =
+    Array.forall(fun coords -> not(containsDuplicates coords board))
 
 /// Whether the sudoku board respects all constraints, i.e. is a valid position
 /// but not necessarily a solution
@@ -98,11 +103,10 @@ let accept (board,_) =
 /// empty elements written a valid value to. If there are no empty elements (board is complete),
 /// the "first" is None.
 let first ((board:int[]),_) =
-    let empty = board |> Array.tryFindIndex(fun elem -> elem = 0)
-    if empty = None then None
-    else
+    match board |> Array.tryFindIndex(fun elem -> elem = 0) with
+    | None -> None
+    | Some(index) ->
         let newBoard = Array.copy board
-        let index = empty.Value
         newBoard.[index] <- 1
         Some(newBoard, index)
 
@@ -122,6 +126,5 @@ let root board = (board, 0)
 
 /// "The sudoku solution" of a board is the board of the first solution.
 let solve board =
-    let rv,_ = Seq.head (backtrack (root board) reject accept first next)
-    rv
+    fst (Seq.head (backtrack (root board) reject accept first next))
    
